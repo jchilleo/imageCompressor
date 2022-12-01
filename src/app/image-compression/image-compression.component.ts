d213016b1d63af9cf81c5feeac74770353b9f0bf
JChilleo <jchilleo2011@gmail.com>
2022-11-30 19:35:45 -0500

    Fixed issue where only a quart of the data was converted.
    Also reduced the size of the ending string by creating a reference, so if a color is used twice, the first index used it referenced instead of writing the same hex code over and over.

--- src/app/image-compression/image-compression.component.ts
+++ src/app/image-compression/image-compression.component.ts
@@ -52,43 +52,78 @@ export class ImageCompressionComponent implements OnInit {
 
   }
 
-  /*
-  Convert the uploaded image to an offscreen canvas item, so we can read the image data without displaying.
+  /**
+   * Convert the uploaded image to an offscreen canvas item, so we can read the image data without displaying.
    */
   async onUpload() {
+    let colorMap = new Map<string, number>();
     var bitmap = await createImageBitmap(this.image);
     var canvas = new OffscreenCanvas(bitmap.width, bitmap.height);
     var context = canvas.getContext('2d');
     context?.drawImage(bitmap,0,0);
     var myData = context?.getImageData(0, 0, bitmap.width, bitmap.height);
-    var hexData = this.convertImageDataToHex(myData as ImageData);
+    var hexData = this.convertImageDataToHex(myData as ImageData, colorMap);
+
+    console.log(hexData);
 
     console.log(`Original: ${getSizeInBytes({obj: myData?.data})} vs Hexdata: ${getSizeInBytes({obj: hexData})}`);
 
   }
 
-  /*
-  Convert image data from rgba to hex.
+  /**
+   * Convert the image data from RGBA to hex values.
+   * @param imageData
+   * @param colorMap
    */
-  convertImageDataToHex(imageData : ImageData) {
+  convertImageDataToHex(imageData : ImageData, colorMap: Map<string, number>) {
 
-    let hexArray = '';
+    let hexArrayAsString = '';
 
-    for(var i = 0; i < imageData.data.length /4 ; i+=4){
-      let hexValue = this.combineRGBAToHexString(imageData.data[i],imageData.data[i+1],imageData.data[i+2],imageData.data[i+3],);
-      hexArray+=(hexValue);
+    for(var i = 0; i < imageData.data.length ; i+=4){
+
+      let hexValue = this.combineRGBAToHexString(imageData.data[i], imageData.data[i+1], imageData.data[i+2], imageData.data[i+3]);
+      hexArrayAsString+=`#${(this.getColorValue(hexValue, Math.floor(i/4), colorMap))}`;
     }
+    console.log(colorMap);
 
-    return(hexArray);
+    return(hexArrayAsString);
 
   }
 
+  /**
+   * This function will utilization memoization by checking if we have already added a color to the string. Or if it's new
+   * color it will record the first index that color was at, so it could be reference later. If a color is repeated we will return the color
+   * as a string with prefix of m. This will be used later when rebuilding the image, so it references the correct color.
+   * @param hexColor - 0-255 color/alpha value of a pixel.
+   * @param index - index the color/alpha value is at.
+   * @param colorMap - map containing all the previous seen colors.
+   */
+  getColorValue(hexColor: string, index: number, colorMap: Map<string, number>) {
+    if(!colorMap.has(hexColor)){
+      colorMap.set(hexColor, index)
+      return hexColor;
+    }
+
+    return `m${colorMap.get(hexColor)}`;
+  }
+
+  /**
+   * Combines the RBGA values into one string.
+   * @param red
+   * @param green
+   * @param blue
+   * @param alpha
+   */
   combineRGBAToHexString (red:number, green: number, blue: number, alpha: number){
 
-    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}${alpha.toString(16)}`;
+    return `${red.toString(16)}${green.toString(16)}${blue.toString(16)}${alpha.toString(16)}`;
   }
 }
 
+/**
+ *
+ * @param obj
+ */
 const getSizeInBytes = ({obj}: { obj: any }) => {
   let str = null;
   if (typeof obj === 'string') {
